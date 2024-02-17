ece2aae781ac78a2b0100b0b2b0dec144516db84
Celine Lee <celine@Celines-Mini.lan>
2024-02-17 18:25:44 -0500

    color in css + title id

--- src/component/App.js
+++ src/component/App.js
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <>
             <div>
-                <h1 className="title" style={{fontFamily:'Waterfall'}}>Hello World - 2024</h1>
+                <h1 className="title" id="title">Hello World - 2024</h1>
                 <h1 className="title" style={{fontFamily:'Ma Shan Zheng'}}>你好世界 - 2024</h1>
                 <p className="title" style={{fontFamily:'Aref Ruqaa'}}>Hello Worldie - 2024</p>
                 <p className="title" style={{fontFamily:'Aref Ruqaa', fontWeight:'bold'}}>Hello Worldie - 2024</p>
