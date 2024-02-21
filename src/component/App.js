d69744c1ecfae8fa5e7cf66b7c92855250267b50
Luca Liu <lucaliu96@gmail.com>
2024-02-21 13:56:21 -0500

    intro section

--- src/component/App.js
+++ src/component/App.js
@@ -11,9 +11,9 @@ const App = () => {
             </div>
 
             {/* First Section - 2 Stadium + 1 Message */}
-            <div style={{border:'1px red dashed',display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center', minHeight:'20rem'}}>
+            <div style={{border:'1px red dashed',display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center', minWidth:'80vw',maxWidth:'99vw', aspectRatio:'4.5/2'}}>
                 {/* First Photo */}
-                <div>
+                <div style={{height:'100%'}}>
                     {/* TODO Stadium 1 */}
                     <StadiumImage/>
                 </div>
--- src/component/StadiumImage.js
+++ src/component/StadiumImage.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const StadiumImage = (props) => {
     return (
-        <div style={{height:'20rem', aspectRatio:'405/775'}}>
-            <h1>Image</h1>
+        <div style={{height:'100%'}}>
+            <img src="../../public/img-wedding-invitation/photo-img/intro1.png" style={{objectFit:'contain', height:'100%'}}/>
         </div>
     );
 };
