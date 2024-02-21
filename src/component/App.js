95006c26d999b4b31ca09fde02632173bf2cf1a6
Luca Liu <lucaliu96@gmail.com>
2024-02-21 15:38:39 -0500

    intro section done

--- src/component/App.js
+++ src/component/App.js
@@ -11,26 +11,26 @@ const App = () => {
             </div>
 
             {/* First Section - 2 Stadium + 1 Message */}
-            <div style={{border:'1px red dashed',display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center', minWidth:'90vw',maxWidth:'99vw', aspectRatio:'4.5/2'}}>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center', minWidth:'90vw',maxWidth:'99vw', aspectRatio:'6/3'}}>
                 {/* First Photo */}
-                <div style={{height:'100%', flex:'2'}}>
+                <div style={{height:'100%', flex:'3', display:'flex', justifyContent:'center'}}>
                     <StadiumImage url='../../public/img-wedding-invitation/photo-img/intro1.png'/>
                 </div>
 
                 {/* Second Phot */}
-                <div style={{height:'100%', flex:'2'}}>
+                <div style={{height:'100%', flex:'3', display:'flex', justifyContent:'center'}}>
                     <StadiumImage url='../../public/img-wedding-invitation/photo-img/intro2.png'/>
                 </div>
 
                 {/* Message */}
-                <div style={{height:'100%', flex:'3',display:'flex', flexDirection:'column', flexWrap:'nowrap', justifyContent:'center', alignItems:'center'}}>
-                    <hr className="horizontal-line" style={{width:'8rem'}}/>
-                    <hr className="horizontal-line" style={{width:'16rem'}}/>
-                    <p>WE ARE GETTING MARRIED</p>
-                    <p>我们要结婚啦</p>
-                    <p><span>October 5th 2024 | </span><span>2024年10月5号</span></p>
-                    <hr className="horizontal-line" style={{width:'16rem'}}/>
-                    <hr className="horizontal-line" style={{width:'8rem'}}/>
+                <div style={{height:'100%', flex:'4',display:'flex', flexDirection:'column', flexWrap:'nowrap', justifyContent:'center', alignItems:'center', maxHeight:'330px'}}>
+                    <hr className="horizontal-line" style={{width:'25%'}}/>
+                    <hr className="horizontal-line" style={{width:'60%'}}/>
+                    <p className="text-english">WE ARE GETTING MARRIED</p>
+                    <p className="text-chinese">我们要结婚啦</p>
+                    <p className="text-english"><span>October 5th 2024 | </span><span className="text-chinese">2024年10月5号</span></p>
+                    <hr className="horizontal-line" style={{width:'60%'}}/>
+                    <hr className="horizontal-line" style={{width:'25%'}}/>
                 </div>
             </div>
 
