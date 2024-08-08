ebf9cb57c36636782ba01b77b2279cc440f0b577
Luca Liu <lucaliu96@gmail.com>
2024-08-08 11:29:33 -0400

    gift populated

--- src/component/Gift.js
+++ src/component/Gift.js
@@ -2,7 +2,22 @@ import React from "react";
 
 const Gift = () => {
     return (
-        <h1>Gift</h1>
+        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', marginBottom:'5rem'}}>
+            <h1 className="title-text-english">Wedding Gift</h1>
+            <div style={{height:'100%', border:'10px solid var(--dark-pink)', borderRadius:'10px', padding:'1rem',
+                display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'
+            }}>
+                <div>
+                    <p className="text-english">Venmo</p>
+                </div>
+                <div>
+                    <p className="text-english">Zelle</p>
+                </div>
+                <div>
+                    <p className="text-english">Costco Gift Card</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
