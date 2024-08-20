b01bd0dee50b354d807214c33f398dd05cb4d8c8
Luca Liu <lucaliu96@gmail.com>
2024-08-20 11:38:29 -0400

    all info in

--- src/component/Gift.js
+++ src/component/Gift.js
@@ -13,22 +13,41 @@ const Gift = () => {
     return (
         <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', marginBottom:'5rem'}}>
             <h1 className="title-text-english">Wedding Gift</h1>
-            <p className="text-english">Your presence at our wedding is the greatest gift of all.</p>
-            <p className="text-english">However, should you wish to honour us with a gift, a list of gift suggestions.</p>
-            <div style={{height:'100%', border:'10px solid var(--dark-pink)', borderRadius:'10px', padding:'1rem',
+            <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', margin:'3rem'}}>
+                <p className="text-english">Your presence at our wedding is the greatest gift of all.</p>
+                <p className="text-english">However, should you wish to honor us with a gift, a list of gift suggestions.</p>
+            </div>
+            <div style={{height:'100%',width:'50vw', border:'10px solid var(--dark-pink)', backgroundColor:'var(--dark-pink)', borderRadius:'10px', padding:'1rem',
                 display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'
             }}>
-                <div onClick={()=>{handleView(venmo, setVenmo)}}>
+                <div style={{margin:'1rem', width:'90%',padding:'1rem', border:'2px solid var(--light-pink)', borderRadius:'10px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}} onClick={()=>{handleView(venmo, setVenmo)}}>
                     <p className="text-english">Venmo</p>
-                    {venmo ? <p>Open Venmo</p> : null}
+                    {venmo ? 
+                        <div style={{}}>
+                            <img src="../../public/MyVenmoQRCode.png" style={{width:'100%', objectFit:'contain'}}/>
+                        </div>
+                    : null}
                 </div>
-                <div onClick={()=>{handleView(zelle, setZelle)}}>
+                <div style={{margin:'1rem', width:'90%',padding:'1rem', border:'2px solid var(--light-pink)', borderRadius:'10px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}} onClick={()=>{handleView(zelle, setZelle)}}>
                     <p className="text-english">Zelle</p>
-                    {zelle ? <p>Open Zelle</p> : null}
+                    {zelle ? 
+                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+                            <p className="text-english" style={{textWrap:'balance', textAlign:'center'}}>Phone: 917-929-1661</p>
+                            <div style={{}}>
+                                <img src="../../public/myZelle.jpg" style={{width:'100%', objectFit:'contain'}}/>
+                            </div>
+                        </div>
+                    : null}
                 </div>
-                <div onClick={()=>{handleView(costco, setCostco)}}>
+                <div style={{margin:'1rem', width:'90%',padding:'1rem', border:'2px solid var(--light-pink)', borderRadius:'10px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}} onClick={()=>{handleView(costco, setCostco)}}>
                     <p className="text-english">Costco Gift Card</p>
-                    {costco ? <p>Open Costco</p> : null}
+                    {costco ? 
+                        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+                            <p className="text-english" style={{textWrap:'balance', textAlign:'center'}}>Email: LUCALIU96@GMAIL.COM</p>
+                            <p className="text-english">OR</p>
+                            <p className="text-english" style={{textWrap:'balance', textAlign:'center'}}>Address: 440 Kent Ave, Apt.4C, Brooklyn, NY 11249</p>
+                        </div>
+                    : null}
                 </div>
             </div>
         </div>
